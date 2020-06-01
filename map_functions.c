8c7c732e118d8594f374c8053d6f3e79fc045eab
Yuvir Sharma <ysharma@student.wethinkcode.co.za>
2020-06-01 13:09:27 +0200

    fixed issues

--- map_functions.c
+++ map_functions.c
@@ -63,6 +63,10 @@ int	add_command(t_map *map, char *command)
 		free(command);
 		return (0);
 	}
+	while (*line == '#')
+	{
+		get_next_line(0, &line);
+	}
 	room = add_rooms(map, line);
 	if (room)
 		*value = room;
@@ -74,22 +78,41 @@ void read_map(t_map *map)
 {
 	char *line;
 	int res;
+	int complete_rooms;
 
 	res = 1;
+	complete_rooms = 0;
 	line = NULL;
 	map->ants = 0;
+	map->num_rooms = 0;
 	while (get_next_line(0, &line) > 0)
 	{
-		if (map->ants == 0)
+		if (map->ants == 0 && set_num(line, &map->ants))
 			set_num(line, &map->ants);
-		else if (*line == '#')
+		else if (*line == '#' && map->ants)
 			res = add_command(map, line);
 		else
 		{
 			if (ft_strchr(line, '-'))
+			{
+				if (!map->start || !map->rooms)
+				{
+					free_everything(map);
+					free(line);
+				}
 				res = add_links(map, line);
+				complete_rooms = 1;
+			}
 			else
+			{
+				if (complete_rooms == 1)
+				{
+					free_everything(map);
+					free(line);
+					error();
+				}
 				res = (add_rooms(map, line) ? 1 : 0);
+			}
 		}
 		if (!res)
 			break ;
